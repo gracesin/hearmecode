9f621a69387346cc42710a95a33ebcc14ac33b99
suellenf <suellen.foth@gmail.com>
2016-03-13 09:57:45 -0400

    Delete lesson2_99bottles.py

--- lesson2_99bottles.py
+++
@@ -1,6 +0,0 @@
-wall = range(99, 0, -1)
-for bottles in wall:
-    print "{0} bottles of beer on the wall, {0} bottles of beer".format(bottles)
-    print "if one of those bottles should happen to fall,  {0} bottles of beer on the wall".format((bottles-1))
-
-    
